2b34cf6477949301e635f01514248ffb9d5ff5e3
Tatiana Rem <tatiana13d@yandex.ru>
2023-04-25 15:02:42 +0600

    Universal methods added

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -11,32 +11,29 @@ public class DashboardPage {
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
     private SelenideElement heading = $("[data-test-id=dashboard]");
-    private static SelenideElement firstCard = $("[data-test-id=\"92df3f1c-a033-48e6-8390-206f6b1f56c0\"]");
-    private static SelenideElement secondCard = $("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"]");
+    private SelenideElement firstCard = $("[data-test-id=\"92df3f1c-a033-48e6-8390-206f6b1f56c0\"]");
+    private SelenideElement secondCard = $("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"]");
     private SelenideElement cashInFirst = $$("[data-test-id=action-deposit]").first();
     private SelenideElement cashInSecond = $$("[data-test-id=action-deposit]").last();
     private SelenideElement reload = $("[data-test-id=action-reload]");
 
-    public TransferMoneyToOtherCardPage cashInFirst() {
-        cashInFirst.click();
+    public TransferMoneyToOtherCardPage cashIn(String cashInType) {
+        if (cashInType.equals("first")) {
+            cashInFirst.click();
+        } else if (cashInType.equals("second")) {
+            cashInSecond.click();
+        } else {
+            throw new IllegalArgumentException("Неверный номер карты " + cashInType);
+        }
         return new TransferMoneyToOtherCardPage();
     }
 
-    public TransferMoneyToOtherCardPage cashInSecond() {
-        cashInSecond.click();
-        return new TransferMoneyToOtherCardPage();
+
+    public int getCurrentBalanceOfCard(SelenideElement card) {
+        String selectedValue = card.getText();
+        String balance = selectedValue.substring(selectedValue.indexOf(balanceStart) + balanceStart.length(),
+                selectedValue.indexOf(balanceFinish));
+        return Integer.parseInt(balance);
     }
 
-
-    public static int getCurrentBalanceOfFirstCard() {
-        String selectedValue = firstCard.getText();
-        String balanceOfFirstCard = selectedValue.substring(29, selectedValue.indexOf(" ", 29));
-        return Integer.parseInt(balanceOfFirstCard);
-    }
-
-    public static int getCurrentBalanceOfSecondCard() {
-        String selectedValue = secondCard.getText();
-        String balanceOfFirstCard = selectedValue.substring(29, selectedValue.indexOf(" ", 29));
-        return Integer.parseInt(balanceOfFirstCard);
-    }
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,22 +1,33 @@
 package ru.netology.web.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPage;
+import ru.netology.web.page.VerificationPage;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
-    @Test
-    void shouldTransferMoneyFromFirstToSecond() {
-        int amount = 1001;
-        var loginPage = open("http://localhost:9999", LoginPage.class);
+    private LoginPage loginPage;
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        var dashboardPage = verificationPage.validVerify(verificationCode);
-        var transferMoneyToOtherCardPage = dashboardPage.cashInSecond();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyFromFirstToSecond() {
+        int amount = 1001;
+        var dashboardPage = new DashboardPage();
+        var transferMoneyToOtherCardPage = dashboardPage.cashIn("second");
         var cardInfo = DataHelper.getFirstCardInfo();
         transferMoneyToOtherCardPage.transferCard(cardInfo, amount);
     }
@@ -24,12 +35,8 @@ class MoneyTransferTest {
     @Test
     void shouldTransferMoneyFromSecondToFirst() {
         int amount = 5000;
-        var loginPage = open("http://localhost:9999", LoginPage.class);
-        var authInfo = DataHelper.getAuthInfo();
-        var verificationPage = loginPage.validLogin(authInfo);
-        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        var dashboardPage = verificationPage.validVerify(verificationCode);
-        var transferMoneyToOtherCardPage = dashboardPage.cashInFirst();
+        var dashboardPage = new DashboardPage();
+        var transferMoneyToOtherCardPage = dashboardPage.cashIn("first");
         var cardInfo = DataHelper.getSecondCardInfo();
         transferMoneyToOtherCardPage.transferCard(cardInfo, amount);
     }
@@ -37,23 +44,17 @@ class MoneyTransferTest {
     @Test
     void shouldTransferMoneyFromFirstCardAndCheckBalance() {
         int amount = 1000;
-        var loginPage = open("http://localhost:9999", LoginPage.class);
-
-        var authInfo = DataHelper.getAuthInfo();
-        var verificationPage = loginPage.validLogin(authInfo);
-        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        var dashboardPage = verificationPage.validVerify(verificationCode);
-
-        int balanceStartFirst = dashboardPage.getCurrentBalanceOfFirstCard();
-        int balanceStartSecond = dashboardPage.getCurrentBalanceOfSecondCard();
-        var transferMoneyToOtherCardPage = dashboardPage.cashInSecond();
+        var dashboardPage = new DashboardPage();
+        int balanceStartFirst = dashboardPage.getCurrentBalanceOfCard($("[data-test-id=\"92df3f1c-a033-48e6-8390-206f6b1f56c0\"]"));
+        int balanceStartSecond = dashboardPage.getCurrentBalanceOfCard($("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"]"));
+        var transferMoneyToOtherCardPage = dashboardPage.cashIn("second");
         var cardInfo = DataHelper.getFirstCardInfo();
         transferMoneyToOtherCardPage.transferCard(cardInfo, amount);
 
         int balanceEndTransferFirstCard = DataHelper.balanceOfFirstCardAfterTransferMoney(balanceStartFirst, amount);
         int balanceEndTransferSecondCard = DataHelper.balanceOfSecondCardAfterTransfer(balanceStartSecond, amount);
-        int balanceOfFirstCardEnd = dashboardPage.getCurrentBalanceOfFirstCard();
-        int balanceOfSecondCardEnd = dashboardPage.getCurrentBalanceOfSecondCard();
+        int balanceOfFirstCardEnd = dashboardPage.getCurrentBalanceOfCard($("[data-test-id=\"92df3f1c-a033-48e6-8390-206f6b1f56c0\"]"));
+        int balanceOfSecondCardEnd = dashboardPage.getCurrentBalanceOfCard($("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"]"));
 
         assertEquals(balanceEndTransferFirstCard, balanceOfFirstCardEnd);
         assertEquals(balanceEndTransferSecondCard, balanceOfSecondCardEnd);
@@ -63,12 +64,8 @@ class MoneyTransferTest {
     @Test
     void shouldTransferMoneyAboveBalance() {
         int amount = 13000;
-        var loginPage = open("http://localhost:9999", LoginPage.class);
-        var authInfo = DataHelper.getAuthInfo();
-        var verificationPage = loginPage.validLogin(authInfo);
-        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        var dashboardPage = verificationPage.validVerify(verificationCode);
-        var transferMoneyToOtherCardPage = dashboardPage.cashInSecond();
+        var dashboardPage = new DashboardPage();
+        var transferMoneyToOtherCardPage = dashboardPage.cashIn("second");
         var cardInfo = DataHelper.getFirstCardInfo();
 
         transferMoneyToOtherCardPage.transferCard(cardInfo, amount);
